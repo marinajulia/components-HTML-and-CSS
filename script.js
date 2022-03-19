717496838bfcef2885217a9dd9014a5c15cc3920
marinajulia <marinajulia707@gmail.com>
2022-03-19 12:57:07 -0300

    chore: spread operator

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ if(typeof localStorage.nome == 'undefined'){
 nome = localStorage.nome;
 
 document.getElementById('nome').innerHTML = nome;
+
+//spread operator para juntar dados de um array em outro ou um objeto em outro:
+
+let pessoa = {
+    nome: "Matheus",
+    idade: 34,
+    cargo: "Programador"
+}
+console.log("pessoa: ", pessoa);
+
+let novaPessoa = {
+    ...pessoa,
+    ano: 200,
+    cidade: "Franca"
+}
+
+console.log("Pessoa nova: ", novaPessoa);
